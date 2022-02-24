d5672d61b05a0acc76cbc206a764ed1555b22aa4
横山 健太 <yokoyama.kenta@tecotec.co.jp>
2022-02-24 12:13:56 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TopGraph />
+  </div>
+</template>
+
+<script>
+import TopGraph from './components/Chart/TopGraph.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TopGraph
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Chart/TopGraph.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="card">
+    上位グラフ
+    <HorizontalBarChart
+      v-if="isLoaded"
+      :chartData="{
+        datasets,
+        labels
+      }"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import HorizontalBarChart from './Base/HorizontalBarChart.vue'
+
+  export default {
+    components: {
+      HorizontalBarChart
+    },
+    data () {
+      return {
+        datasets: [],
+        labels: [],
+        isLoaded: false
+      }
+    },
+    async mounted () {
+      await this.getData()
+      this.isLoaded = true
+    },
+    computed: {
+      options() {
+        return {
+          responsive: true,
+          scales: {
+            xAxes: [
+              {
+                gridLines: {
+                  color: "transparent",
+                  zeroLineColor: "#E6E6E6",
+
+                  // display: false,
+                  drawTicks: false
+                  // drawOnChartArea: false
+                  // drawBorder: false,
+                  // display: false,
+                  // display: false,
+                  // drawTicks: true,
+                  // drawBorder: false,
+                },
+                ticks: {
+                  beginAtZero: true,
+                  callback: (value, index) => {
+                    return index === 0 ? `${value}万円` : value.toLocaleString()
+                  }
+                }
+              },
+            ],
+            yAxes: [
+              {
+                gridLines: {
+                  color: "transparent",
+                  zeroLineColor: "#E6E6E6",
+                  drawOnChartArea: false,
+
+                  // display: false,
+                  // drawOnChartArea: false
+                  drawTicks: false
+                  // display: false,
+                  // drawTicks: true
+                },
+                ticks: {
+                }
+              },
+              {
+                gridLines: {
+                  color: "transparent",
+                  zeroLineColor: "#E6E6E6",
+                  drawTicks: false
+                },
+                ticks: {
+                  display: false
+                }
+              },
+            ]
+          },
+          legend: {
+            display: false
+          },
+          tooltips:{
+            mode:'label' //マウスオーバー時に表示されるtooltip
+          }
+        }
+      }
+    },
+    methods: {
+      async getData () {
+        // 月ごとにリクエスト
+        const chartData = await axios.get('/mock/chart/topGraph.json')
+          .then(response => {
+            // 降順にソートして返す
+            return response.data.sort(
+              (a,b) => {
+                return (a.data < b.data ? 1 : -1)
+              }
+            )
+          })
+          .catch(error => console.log(error))
+
+        this.datasets = [{
+          label: '売上',
+          data: chartData.map(item => {
+            return item.data
+          }),
+          backgroundColor: "#0EBAB6",
+          barPercentage: 0.3,
+        }]
+
+        this.labels = chartData.map(item => {
+          return item.name
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+.card {
+  width: 400px;
+  height: 400px;
+}
+</style>
