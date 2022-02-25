683541b08a9e57c10da28d1b2a5cb618f7c71521
横山 健太 <yokoyama.kenta@tecotec.co.jp>
2022-02-25 18:02:48 +0900

    微調整

--- src/components/Chart/Transition.vue
+++ src/components/Chart/Transition.vue
@@ -25,6 +25,7 @@ export default {
     return {
       datasets: [],
       labels: [],
+      options: {},
       isLoaded: false,
     };
   },
@@ -32,9 +33,35 @@ export default {
     await this.getData();
     this.isLoaded = true;
   },
-  computed: {
-    options() {
-      return {
+  computed: {},
+  methods: {
+    async getData() {
+      // 月ごとにリクエスト
+      const chartData = await axios
+        .get("/mock/chart/transition.json")
+        .then((response) => {
+          return response.data;
+        })
+        .catch((error) => console.log(error));
+
+      this.datasets = chartData.data.map((item) => {
+        return {
+          label: item.name,
+          borderColor: item.color,
+          backgroundColor: "rgba(255,0,0,0)",
+          data: item.data,
+          lineTension: 0,
+          borderWidth: 1.5,
+          pointRadius: 0,
+          pointHitRadius: 10,
+        };
+      });
+
+      this.labels = chartData.data[0].data.map((item, index) => {
+        return moment(chartData.start).set("month", index).format("YYYY-MM-DD");
+      });
+
+      this.options = {
         responsive: true,
         maintainAspectRatio: false,
         scales: {
@@ -103,47 +130,32 @@ export default {
           labels: {
             padding: 35,
             boxWidth: 30,
-            boxHeight: 3,
-            // fillStyle: this.datasets[0].color,
-            generateLabels: function (data) {
-              console.log(data);
-              // 凡例の表示
-              // const chartData = data.tooltip._data;
-              // const chartDataset = chartData.datasets[0];
-              // const chartLabels = chartData.labels;
+            generateLabels: () => {
+              const legends = this.datasets.map(data => {
+                return {
+                  text: chartData.data.find(item => item.name === data.label).order,
+                  // 凡例ボックスの塗りつぶしスタイル
+                  fillStyle: data.borderColor,
+                  //  trueの場合、この項目は非表示のデータセットを表します。ラベルは取り消し線を伴ってレンダリングされます
+                  hidden: false,
+                  // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en/docs/Web/API/CanvasRenderingContext2D/lineCap
+                  lineCap: "",
+                  // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash
+                  lineDash: [0],
+                  // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineDashOffset
+                  lineDashOffset: 0,
+                  // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineJoin
+                  lineJoin: "bevel",
+                  // 枠線の幅
+                  lineWidth: 0,
+                  // 凡例ボックスのストロークスタイル
+                  strokeStyle: "transparent",
+                  // 凡例ボックスのポイントスタイル（usePointStyleがtrueの場合にのみ使用されます）
+                  pointStyle: ""
+                }
+              })
 
-              const legendArray = [];
-
-              // for (let i = 0; i < chartLabels.length; i++) {
-              //   const eachLabel = chartLabels[i];
-              //   const eachData = chartDataset.data[i];
-              //   const backgroundColor = chartDataset.backgroundColor[i];
-
-              //   const eachLengend = {
-              //       // 表示されるラベル
-              //       text: `${eachLabel} ( ${eachData} )`,
-              //       // 凡例ボックスの塗りつぶしスタイル
-              //       fillStyle: backgroundColor,
-              //       //  trueの場合、この項目は非表示のデータセットを表します。ラベルは取り消し線を伴ってレンダリングされます
-              //       hidden: false,
-              //       // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en/docs/Web/API/CanvasRenderingContext2D/lineCap
-              //       lineCap: "square",
-              //       // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash
-              //       lineDash: [0],
-              //       // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineDashOffset
-              //       lineDashOffset: 0,
-              //       // ボックス枠用。次をご覧ください。https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineJoin
-              //       lineJoin: "bevel",
-              //       // 枠線の幅
-              //       lineWidth: 1,
-              //       // 凡例ボックスのストロークスタイル
-              //       strokeStyle: "",
-              //       // 凡例ボックスのポイントスタイル（usePointStyleがtrueの場合にのみ使用されます）
-              //       pointStyle: ""
-              //   };
-              //   legendArray.push(eachLengend);
-              // } // end for
-              return legendArray;
+              return legends;
             },
           },
         },
@@ -151,44 +163,8 @@ export default {
           mode: "label", //マウスオーバー時に表示されるtooltip
         },
       };
-    },
-  },
-  methods: {
-    async getData() {
-      // 月ごとにリクエスト
-      const chartData = await axios
-        .get("/mock/chart/transition.json")
-        .then((response) => {
-          // 合計を降順にソートして返す
-          response.data.data.sort((a, b) => {
-            const totalA = a.data.reduce((sum, element) => {
-              return sum + element;
-            }, 0);
-            const totalB = b.data.reduce((sum, element) => {
-              return sum + element;
-            }, 0);
-            return totalA < totalB ? 1 : -1;
-          });
-          return response.data;
-        })
-        .catch((error) => console.log(error));
 
-      this.datasets = chartData.data.map((item) => {
-        return {
-          label: item.name,
-          borderColor: item.color,
-          backgroundColor: "rgba(255,0,0,0)",
-          data: item.data,
-          lineTension: 0,
-          borderWidth: 1.5,
-          pointRadius: 0,
-          pointHitRadius: 10,
-        };
-      });
 
-      this.labels = chartData.data[0].data.map((item, index) => {
-        return moment(chartData.start).set("month", index).format("YYYY-MM-DD");
-      });
     },
   },
 };
